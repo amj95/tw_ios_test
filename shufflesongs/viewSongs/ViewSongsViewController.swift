953d227e416ef17e63b3be37f08aa65b5d059de3
Antonio Marcos <amoj@ecomp.poli.br>
2019-09-16 10:26:14 -0300

    added lauchscreen+colors

---
+++ shufflesongs/viewSongs/ViewSongsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewSongsViewController.swift
+//  shufflesongs
+//
+//  Created by antonio marcos on 16/09/2019.
+//  Copyright © 2019 tw. All rights reserved.
+//
+
+import UIKit
+class ViewSongsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
