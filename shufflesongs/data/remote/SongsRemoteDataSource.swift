2ea1cf9f597fdb57964b98e67f370f0f4e39faf9
Antonio Marcos <amoj@ecomp.poli.br>
2019-09-21 14:10:35 -0300

    created remoteDataSource

--- shufflesongs/data/remote/SongsRemoteDataSource.swift
+++ shufflesongs/data/remote/SongsRemoteDataSource.swift
@@ -8,6 +8,39 @@
 
 import Foundation
 
-classs SongsRemoteDataSource{
+class SongsRemoteDataSource{
     
+    private static let basePath = "https://us-central1-tw-exercicio-mobile.cloudfunctions.net/lookup?id="
+    private static let configuration: URLSessionConfiguration = {
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = 30.0
+        return config
+    }()
+    private static let session = URLSession(configuration: configuration)
+    
+    class func getSongs(){
+        guard let url = URL(string: basePath) else {return}
+        
+        let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error:
+            Error?) in
+            if error == nil{
+                
+                guard let response = response as? HTTPURLResponse else {return}
+                if response.statusCode == 200{
+                    guard let data = data else {return}
+                    do{
+                        let dataResponse = try JSONDecoder().decode(SongsResponse.self, from: data)
+                        print(dataResponse)
+                    }catch{
+                        print(error.localizedDescription)
+                    }
+                }else{
+                    print("Invalid server code")
+                }
+            } else{
+                print(error!)
+            }
+    }
+        dataTask.resume()
+    }
 }
