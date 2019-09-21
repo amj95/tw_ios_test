a746cdbfb8fffd6f10edb1ec7ea255541a692dc3
Antonio Marcos <amoj@ecomp.poli.br>
2019-09-21 13:25:32 -0300

    added viewSongs requirement

---
+++ shufflesongs/data/remote/SongsRemoteDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  SongsRemoteDataSource.swift
+//  shufflesongs
+//
+//  Created by antonio marcos on 21/09/2019.
+//  Copyright © 2019 tw. All rights reserved.
+//
+
+import Foundation
+
+classs SongsRemoteDataSource{
+    
+}
---
+++ shufflesongsTests/ViewSongsTests.swift
@@ -0,0 +1,21 @@
+//
+//  shufflesongsTests.swift
+//  shufflesongsTests
+//
+//  Created by antonio marcos on 16/09/2019.
+//  Copyright © 2019 tw. All rights reserved.
+//
+
+import XCTest
+@testable import shufflesongs
+
+class ViewSongsTest: XCTestCase {
+
+    var collection: ViewSongsViewController!
+    
+    func testExpectedOrderItems(){
+        
+        
+    }
+
+}
