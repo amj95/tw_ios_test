0a50c683851506d4c42c5ae38768184052c210a1
Antonio Marcos <amoj@ecomp.poli.br>
2019-09-25 10:31:23 -0300

    simple UI test

--- shufflesongsUITests/shufflesongsUITests.swift
+++ shufflesongsUITests/shufflesongsUITests.swift
@@ -27,8 +27,7 @@ class shufflesongsUITests: XCTestCase {
     }
 
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCUIApplication().navigationBars["ShuffleSongs"].buttons["Shuffle"].tap()
     }
 
 }
